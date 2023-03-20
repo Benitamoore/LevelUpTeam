6707fbc5df06ae81806315c670f49b6a7c61bbf5
benitamoore <benitaresto09@comcast.net>
2023-03-20 14:09:29 +0000

    Benita made changes to this file

--- linux.sh
+++ linux.sh
@@ -5,13 +5,13 @@ read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" Benita 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" && March 20, 2023
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
